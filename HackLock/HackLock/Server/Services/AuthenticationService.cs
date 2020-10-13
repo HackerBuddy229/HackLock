c2ddb2a5503b15163e898389644459742abd6c4f
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-10-13 17:58:09 +0200

    Finished Auth service. Building asp.net core 3.1 windows 10

---
+++ HackLock/HackLock/Server/Services/AuthenticationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackLock.Server.Data.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HackLock.Server.Services
+{
+    public interface IAuthenticationService
+    {
+        public Task<bool> AuthenticateUser(string username, string password);
+    }
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly SignInManager<ApplicationIdentityUser> _signInManager;
+
+        public AuthenticationService(SignInManager<ApplicationIdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+
+        public async Task<bool> AuthenticateUser(string username, string password)
+        {
+            var user = 
+                _signInManager.UserManager.Users
+                    .FirstOrDefault(u => u.UserName == username);
+            if (user == null)
+                return false;
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+            return result.Succeeded;
+        }
+    }
+}
